94385d8fabe32d0da6da09c2e5f8217a5b9d5348
Aada Riihimäki <aada.e.riihimaki@student.jyu.fi>
2026-03-31 17:17:08 +0300

    lisätty pohjat postikorteille

--- script.js
+++ script.js
@@ -3,17 +3,32 @@ const matkat = [
         kohde: "Pariisi, Ranska",
         hotelliUrl: "https://www.ritzparis.com",
         kuva: "https://picsum.photos/id/1011/500/400",
-        terveiset: "Rakkautta ilmassa! Kävin Eiffelin juurella ja nautin maailman parhaista leivoksista. Hotelli oli aivan uskomaton."
+        terveiset: "Ihana kaupunki! Söin liikaa croissantteja ja ihastuin Eiffeliin. Hotelli oli upea!"
     },
     {
         kohde: "Tokio, Japani",
         hotelliUrl: "https://www.parkhyatttokyo.com",
         kuva: "https://picsum.photos/id/1016/500/400",
-        terveiset: "Täällä on aina vilinää! Valot, ruoka ja kulttuuri ovat jotain aivan muuta. En haluaisi lähteä kotiin."
+        terveiset: "Neonvaloja ja maailman parasta sushia. Täällä on aina jotain uutta nähtävää!"
+    },
+    {
+        kohde: "Rooma, Italia",
+        hotelliUrl: "https://www.hoteldebussie.com",
+        kuva: "https://picsum.photos/id/1033/500/400",
+        terveiset: "Parasta pizzaa ja gelatoa! Colosseum oli livenä vielä hienompi kuin kuvissa."
+    },
+    {
+        kohde: "Lappi, Suomi",
+        hotelliUrl: "https://www.kakslauttanen.fi",
+        kuva: "https://picsum.photos/id/1029/500/400",
+        terveiset: "Revontulia ja pakkasta! Iglussa nukkuminen oli aivan unohtumaton kokemus."
     }
 ];
 
+// Alapuolella oleva koodi pysyy samana kuin aiemmin:
 const container = document.getElementById('postikortit-container');
+
+container.innerHTML = ''; // Tyhjennetään kontti varmuuden vuoksi
 
 matkat.forEach(matka => {
     const kortti = document.createElement('div');
